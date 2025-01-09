8754418f2d77d16cae1754dd579c3648dabe2aea
FCL00 <flagahit00@gmail.com>
2025-01-09 23:28:03 +0800

    add product form route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Home from "@/pages/Home/Home";
 import Login from "@/pages/Auth/Login";
 import Signup from "@/pages/Auth/Signup";
 import Cart from "@/pages/Cart/Cart";
+import Product from "@/pages/Product/Product";
 
 export const router = createBrowserRouter([
 
@@ -11,4 +12,5 @@ export const router = createBrowserRouter([
     { path: "/login", element: <Login />, },
     { path: "/signup", element: <Signup />, },
     { path: "/cart", element: <Cart />, },
+    { path: "/product/", element: <Product />}
 ]);
